31976e39df24b1e359b36b4d0593e8330b0ea2ea
CassianeVitoria <127560232+CassianeVitoria@users.noreply.github.com>
2024-05-24 00:16:05 +0000

    23/05

---
+++ avalia-ocfp/resources/views/MyPortfolio/Components/titulo.blade.php
@@ -0,0 +1,10 @@
+<body>
+  <nav class="navbar navbar-light custom-navbar">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">MyPortfolio.</a>
+      <a href="#" class="burger" data-bs-toggle="collapse" data-bs-target="#main-navbar">
+        <span></span>
+      </a>
+    </div>
+  </nav>
+ <main id="main"> 
--- avalia-ocfp/resources/views/MyPortfolio/index.blade.php
+++ avalia-ocfp/resources/views/MyPortfolio/index.blade.php
@@ -1,13 +1,4 @@
 @extends('MyPortfolio/layouts')
-@section('menu')
-@extends('MyPortfolio/Components/menu')
 
-@extends('MyPortfolio/layouts')
-@section('corpo')
-@extends('MyPortfolio/Components/corpo')
 
-@extends('MyPortfolio/layouts')
-@section('css')
-@extends('MyPortfolio/Components/css')
 
-@endsection
